6b4c98631e9a8ec94568c4028c02b79765ba86c5
Francis David Okon <francisdavidokon77@gmail.com>
2026-06-16 16:28:25 +0100

    Added tutor's student enpoint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import app from "../lib/express.js";
 import cookieparser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import coursesRoutes from "./routes/courses.route.js";
+import tutorStudents from "./routes/tutor.student.route.js";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(cookieparser());
 
 app.use("/auth", authRoutes);
 app.use("/courses", coursesRoutes);
+app.use("/tutors-student", tutorStudents);
 
 const PORT = 8080;
 app.listen(PORT, () => {
