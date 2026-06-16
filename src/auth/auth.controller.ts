c85de8a1105674003211d82cc2e139f11888d743
Francis David Okon <francisdavidokon77@gmail.com>
2026-06-16 04:31:02 +0100

    Update: Completed authflow [register, login, verify-email, resend-verification-email, forgot-password, reset-password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,48 @@ import { registerSchema } from "../../lib/validation/auth.js";
 import { loginSchema } from "../../lib/validation/auth.js";
 import { prisma } from "../../lib/prisma.js";
 import { hashPassword } from "../../lib/auth/password.js";
-import { generateToken } from "../../lib/auth/jwt.js";
+import { generateToken, verifyToken } from "../../lib/auth/jwt.js";
 import { comparePassword } from "../../lib/auth/password.js";
-import { sendVerificationEmail } from "../../lib/mail/mail.js";
+import {
+  sendResetPasswordEmail,
+  sendVerificationEmail,
+} from "../../lib/mail/mail.js";
 import { router } from "../../lib/express.js";
 import type { Request, Response } from "express";
+
+// Get current user endpoint
+router.get("/me", async (req: Request, res: Response) => {
+  try {
+    // get token from cookies header
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({ error: "Missing token" });
+    }
+
+    // Verify token
+    const decoded = verifyToken(token);
+    const userId = typeof decoded === "string" ? decoded : decoded.id;
+
+    // Fetch user using the verified token
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+
+      select: {
+        fullName: true,
+        email: true,
+        role: true,
+        isVerified: true,
+      },
+    });
+
+    return res.status(200).json({ success: true, user });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 // Register enpoint
 router.post("/register", async (req: Request, res: Response) => {
@@ -121,10 +158,191 @@ router.post("/login", async (req: Request, res: Response) => {
       maxAge: 60 * 60 * 24 * 7,
     });
 
-    res.status(200).json({ message: "Login successfull" });
+    return res.status(200).json({ message: "Login successfull" });
   } catch (error: any) {
     console.log(error);
     res.status(500).json({ error: error.message });
+  }
+});
+
+// Verify email endpoint
+router.put("/confirm-email", async (req: Request, res: Response) => {
+  try {
+    const searchQuery = req.query;
+    const token = searchQuery.token as string;
+
+    if (!token) {
+      return res.status(400).json({ error: "Invalid Token" });
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {
+        verificationToken: token,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "Unauthorized" });
+    }
+
+    if (
+      user.verificationTokenExpiresAt &&
+      user.verificationTokenExpiresAt < new Date()
+    ) {
+      return res.status(400).json({ error: "Verification link expired" });
+    }
+
+    if (user.isVerified) {
+      return res
+        .status(200)
+        .json({ message: "Your email is already verified" });
+    }
+
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        isVerified: true,
+      },
+    });
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Email verified successfully!" });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Resend verification email endpoint
+router.post("/resend-email", async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const { id: userId, email } = body;
+
+    const verificationToken = crypto.randomUUID();
+
+    const user = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        verificationToken,
+        verificationTokenExpiresAt: new Date(Date.now() + 1000 * 60 * 60),
+      },
+    });
+
+    const studentName = user.fullName.split(" ")[0] as string;
+
+    await sendVerificationEmail(email, verificationToken, studentName);
+
+    return res.status(200).json({
+      success: true,
+      message: `Email resent successfully to ${email}`,
+    });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Forgot password endpoint
+router.post("/forgot-password", async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const { email } = body;
+
+    const resetPasswordToken = crypto.randomUUID();
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({
+        error: "User with this email does not exist",
+      });
+    }
+
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        resetPasswordToken,
+        resetPasswordTokenExpiresAt: new Date(Date.now() + 30 * 60 * 1000),
+      },
+    });
+
+    const name = user.fullName.split(" ")[0] as string;
+    await sendResetPasswordEmail(name, resetPasswordToken, user.email);
+
+    return res.status(200).json({
+      success: true,
+      message: `We've sent a password reset link to ${user.email} Please check your inbox and follow the instructions.`,
+    });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Reset password endpoint
+router.put("/reset-password", async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const { token, password } = body;
+
+    if (!token) {
+      return res.status(404).json({ error: "Missing token" });
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {
+        resetPasswordToken: token,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "Unauthorized!" });
+    }
+
+    if (
+      user.resetPasswordTokenExpiresAt &&
+      user.resetPasswordTokenExpiresAt < new Date()
+    ) {
+      return res.status(410).json({ error: "Verificaiton link expired" });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    await prisma.user.update({
+      where: {
+        email: user.email,
+      },
+      data: {
+        password: hashedPassword,
+        resetPasswordToken: null,
+        resetPasswordTokenExpiresAt: null,
+      },
+
+      select: {
+        id: true,
+      },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Password reset successful",
+      user,
+    });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Logout endpoint
+router.get("/logout", async (req: Request, res: Response) => {
+  try {
+    res.clearCookie("token");
+    return res.status(204).json({ message: "Logged out successfully" });
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
   }
 });
 
